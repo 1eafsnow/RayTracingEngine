cdacae749536a8e589cfe6cc185124a08912eec1
1eafsnow <46445616+1eafsnow@users.noreply.github.com>
2026-09-01 20:07:08 +0800

    Add GUI object inspector state

--- src/GUI/GUI.h
+++ src/GUI/GUI.h
@@ -15,24 +15,31 @@ static void glfw_error_callback(int error, const char* description)
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
+enum class SelectedObjectType
+{
+    None,
+    Sphere,
+    Triangle,
+    Quadrilateral,
+    Light
+};
+
 class GUI
 {
 public:
+    static constexpr int SidePanelWidth = 360;
+
     int width = 0;
     int height = 0;
-
     int pixelWidth = 0;
     int pixelHeight = 0;
 
     GLFWwindow* glWindow = nullptr;
     ImGuiIO* io = nullptr;
-
     Renderer* renderer = nullptr;
     GLuint pixelBufferObjects[Renderer::OpenGLPixelBufferCount]{};
     GLuint renderTextures[Renderer::OpenGLPixelBufferCount]{};
     int displayTextureIndex = 0;
-
-    bool objectWindow = false;
 
     void DrawPixels(unsigned char* pixels);
     void SetRenderer(Renderer* renderer);
@@ -46,8 +53,18 @@ private:
     bool cameraLookActive = false;
     double lastMouseX = 0.0;
     double lastMouseY = 0.0;
+    SelectedObjectType selectedObjectType = SelectedObjectType::None;
+    int selectedObjectIndex = -1;
 
     void UpdateRenderTexture();
     void UpdateCameraControls(float deltaTime);
+    void UpdateCameraProjection();
+    void HandleObjectPicking();
+    void PickObject(double mouseX, double mouseY);
+    void DrawSettingsPanel();
+    void DrawObjectInspectorPanel();
+    void DrawPerformancePanel();
+    void SyncSelectedGeometry();
+    void SyncMaterial(int materialIndex);
     bool IsCursorInRenderViewport(double x, double y) const;
 };
