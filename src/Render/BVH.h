c174cdb2c223315fc47b64766307a7cf928c3ab3
1eafsnow <46445616+1eafsnow@users.noreply.github.com>
2026-09-01 14:13:15 +0800

    Add compact BVH node type for GPU traversal

---
+++ src/Render/BVH.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Math/Vector.h>
+
+struct BVHNode
+{
+    Vector3 boundsMin;
+    Vector3 boundsMax;
+    int left = -1;
+    int right = -1;
+    int start = 0;
+    int count = 0;
+
+    __host__ __device__ bool IsLeaf() const
+    {
+        return count > 0;
+    }
+};
