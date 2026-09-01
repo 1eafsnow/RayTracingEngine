04a71be24edd10837d5359a445abc65bc15bf772
1eafsnow <46445616+1eafsnow@users.noreply.github.com>
2026-09-01 10:27:15 +0800

    Fix CUDA device constants and const rotator methods

--- src/Math/Rotator.h
+++ src/Math/Rotator.h
@@ -22,113 +22,55 @@ public:
 
 	static const _Rotator Zero;
 
-	__host__ __device__ _Rotator operator+(const _Rotator& r)
-	{
-		return _Rotator(yaw + r.yaw, pitch + r.pitch, roll + r.roll);
-	}
+	__host__ __device__ _Rotator operator+(const _Rotator& r) const { return _Rotator(yaw + r.yaw, pitch + r.pitch, roll + r.roll); }
+	__host__ __device__ _Rotator operator-(const _Rotator& r) const { return _Rotator(yaw - r.yaw, pitch - r.pitch, roll - r.roll); }
+	__host__ __device__ _Rotator operator*(const _Rotator& r) const { return _Rotator(yaw * r.yaw, pitch * r.pitch, roll * r.roll); }
+	__host__ __device__ _Rotator operator/(const _Rotator& r) const { return _Rotator(yaw / r.yaw, pitch / r.pitch, roll / r.roll); }
+	__host__ __device__ _Rotator operator*(const float& s) const { return _Rotator(yaw * s, pitch * s, roll * s); }
+	__host__ __device__ _Rotator operator/(const float& s) const { return _Rotator<T>(yaw / s, pitch / s, roll / s); }
+	__host__ __device__ _Rotator operator-() const { return _Rotator(-yaw, -pitch, -roll); }
+	__host__ __device__ void operator=(const _Rotator& r) { memcpy(this, &r, sizeof(_Rotator)); }
+	__host__ __device__ bool operator==(const _Rotator& r) const { return yaw == r.yaw && pitch == r.pitch && roll == r.roll; }
+	__host__ __device__ bool operator!=(const _Rotator& r) const { return !(*this == r); }
+	__host__ __device__ T& operator[](const int& i) { return (&yaw)[i]; }
+	__host__ __device__ const T& operator[](const int& i) const { return (&yaw)[i]; }
 
-	__host__ __device__ _Rotator operator-(const _Rotator& r)
+	__host__ __device__ _Matrix3<T> RotateMatrix() const
 	{
-		return _Rotator(yaw - r.yaw, pitch - r.pitch, roll - r.roll);
-	}
+		float yawRad = yaw / 180.0f * PI;
+		float pitchRad = pitch / 180.0f * PI;
+		float rollRad = roll / 180.0f * PI;
 
-	__host__ __device__ _Rotator operator*(const _Rotator& r)
-	{
-		return _Rotator(yaw * r.yaw, pitch * r.pitch, roll * r.roll);
-	}
-
-	__host__ __device__ _Rotator operator/(const _Rotator& r)
-	{
-		return _Rotator(yaw / r.yaw, pitch / r.pitch, roll / r.roll);
-	}
-
-	__host__ __device__ _Rotator operator*(const float& s)
-	{
-		return _Rotator(yaw * s, pitch * s, roll * s);
-	}
-
-	__host__ __device__ _Rotator operator/(const float& s)
-	{
-		return _Rotator<T>(yaw / s, pitch / s, roll / s);
-	}
-
-	__host__ __device__ _Rotator operator-()
-	{
-		return _Rotator(-yaw, -pitch, -roll);
-	}
-
-	__host__ __device__ void operator=(const _Rotator& r)
-	{
-		memcpy(this, &r, sizeof(_Rotator));
-	}
-
-	__host__ __device__ bool operator==(const _Rotator& r)
-	{
-		return yaw == r.yaw && pitch == r.pitch && roll == r.roll ? true : false;
-	}
-
-	__host__ __device__ bool operator!=(const _Rotator& r)
-	{
-		return yaw == r.yaw && pitch == r.pitch && roll == r.roll ? false : true;
-	}
-
-	__host__ __device__ T& operator[](const int& i)
-	{
-		return yaw;
-	}
-
-	__host__ __device__ _Matrix3<T> RotateMatrix()
-	{
-		float yaw = this->yaw / 180.0 * PI;
-		float pitch = this->pitch / 180.0 * PI;
-		float roll = this->roll / 180.0 * PI;
-
-		return _Matrix3<T>(cos(roll) * cos(yaw) - sin(roll) * -sin(pitch) * -sin(yaw), sin(roll) * cos(pitch), cos(roll) * sin(yaw) - sin(roll) * -sin(pitch) * cos(yaw),
-			-sin(roll) * cos(yaw) + cos(roll) * sin(pitch) * -sin(yaw), cos(roll) * cos(pitch), -sin(roll) * sin(yaw) + cos(roll) * sin(pitch) * cos(yaw),
-			cos(pitch) * -sin(yaw), -sin(pitch), cos(pitch) * cos(yaw));
+		return _Matrix3<T>(cos(rollRad) * cos(yawRad) - sin(rollRad) * -sin(pitchRad) * -sin(yawRad), sin(rollRad) * cos(pitchRad), cos(rollRad) * sin(yawRad) - sin(rollRad) * -sin(pitchRad) * cos(yawRad),
+			-sin(rollRad) * cos(yawRad) + cos(rollRad) * sin(pitchRad) * -sin(yawRad), cos(rollRad) * cos(pitchRad), -sin(rollRad) * sin(yawRad) + cos(rollRad) * sin(pitchRad) * cos(yawRad),
+			cos(pitchRad) * -sin(yawRad), -sin(pitchRad), cos(pitchRad) * cos(yawRad));
 	}
 
 	__host__ __device__ _Rotator(T yaw = 0, T pitch = 0, T roll = 0) : yaw(yaw), pitch(pitch), roll(roll) { }
 
-	__host__ __device__ _Quaternion<T> Quaternion()
+	__host__ __device__ _Quaternion<T> Quaternion() const
 	{
-		_Vector3<T> Forward(1, 0, 0);
-		_Vector3<T> Leftward(0, -1, 0);
-		_Vector3<T> Upward(0, 0, 1);
+		_Vector3<T> forward(1, 0, 0);
+		_Vector3<T> leftward(0, -1, 0);
+		_Vector3<T> upward(0, 0, 1);
 
 		_Quaternion<T> qYaw;
-		qYaw.SetRotation(Upward, yaw / 180.0f * PI);
-		//result = qYaw.Rotate(result);
-		Forward = qYaw.Rotate(Forward);
-		Leftward = qYaw.Rotate(Leftward);
-		//Upward = qYaw.Rotate(Upward);
+		qYaw.SetRotation(upward, yaw / 180.0f * PI);
+		forward = qYaw.Rotate(forward);
+		leftward = qYaw.Rotate(leftward);
 
 		_Quaternion<T> qPitch;
-		qPitch.SetRotation(Leftward, pitch / 180.0f * PI);
-		//result = qPitch.Rotate(result);
-		Forward = qPitch.Rotate(Forward);
-		//Leftward = qPitch.Rotate(Leftward);
-		//Upward = qPitch.Rotate(Upward);
+		qPitch.SetRotation(leftward, pitch / 180.0f * PI);
+		forward = qPitch.Rotate(forward);
 
 		_Quaternion<T> qRoll;
-		qRoll.SetRotation(Forward, roll / 180.0f * PI);
-		//result = qRoll.Rotate(result);
-		//Forward = qRoll.Rotate(Forward);
-		//Leftward = qRoll.Rotate(Leftward);
-		//Upward = qRoll.Rotate(Upward);
+		qRoll.SetRotation(forward, roll / 180.0f * PI);
 
 		return Cross(qRoll, Cross(qPitch, qYaw));
 	}
 
-	__host__ __device__ _Vector3<T> Rotate(_Vector3<T> v)
-	{
-		return Quaternion().Rotate(v);
-	}
-
-	__host__ __device__ void Print()
-	{
-		printf("(yaw: %f, pitch: %f, roll: %f)\n", yaw, pitch, roll);
-	}
+	__host__ __device__ _Vector3<T> Rotate(_Vector3<T> v) const { return Quaternion().Rotate(v); }
+	__host__ __device__ void Print() const { printf("(yaw: %f, pitch: %f, roll: %f)\n", yaw, pitch, roll); }
 };
 
 using RotatorF = _Rotator<float>;
--- src/Render/Ray.h
+++ src/Render/Ray.h
@@ -10,13 +10,13 @@ public:
 	Vector3 location;
 	Vector3 direction;
 	int depth;
-	
+
 	__host__ __device__ Ray(Vector3 location = Vector3(0, 0, 0), Vector3 direction = Vector3(0, 0, 0));
 };
 
 struct RaySampleResult
 {
-	Vector3 brdf = Vector3::Zero;
+	Vector3 brdf = Vector3(0.0f, 0.0f, 0.0f);
 	float pdf = 0.0f;
 	float cosine = 0.0f;
 	float attenuation = 1.0f;
@@ -26,9 +26,9 @@ struct RayHitResult
 {
 	bool isHit = false;
 	float distance = FLT_MAX;
-	Vector3 location = Vector3::Zero;
-	Vector3 normal = Vector3::Zero;
+	Vector3 location = Vector3(0.0f, 0.0f, 0.0f);
+	Vector3 normal = Vector3(0.0f, 0.0f, 0.0f);
 	class Material* material = nullptr;
-	Vector3 color = Vector3::Zero;
+	Vector3 color = Vector3(0.0f, 0.0f, 0.0f);
 	int objectId = -1;
 };
