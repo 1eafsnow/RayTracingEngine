aaa1f5507627ba51d8b280d9813b8409d7f9ba41
1eafsnow <46445616+1eafsnow@users.noreply.github.com>
2026-09-01 09:28:15 +0800

    Add renderer cleanup

--- src/Render/Renderer.h
+++ src/Render/Renderer.h
@@ -15,72 +15,70 @@ template <typename T>
 class Binding
 {
 public:
-	T* host;
-	T* dev;
+    T* host;
+    T* dev;
 
-	Binding(T* host, T* dev)
-	{
-		this->host = host;
-		this->dev = dev;
-	}
+    Binding(T* host, T* dev) : host(host), dev(dev) {}
 
-	void HostToDevice()
-	{
-		cudaMemcpy(dev, host, sizeof(T), cudaMemcpyKind::cudaMemcpyHostToDevice);
-	}
+    void HostToDevice()
+    {
+        cudaMemcpy(dev, host, sizeof(T), cudaMemcpyHostToDevice);
+    }
 
-	void DeviceToHost()
-	{
-		cudaMemcpy(host, dev, sizeof(T), cudaMemcpyKind::cudaMemcpyDeviceToHost);
-	}
+    void DeviceToHost()
+    {
+        cudaMemcpy(host, dev, sizeof(T), cudaMemcpyDeviceToHost);
+    }
 };
 
 class Renderer
 {
 public:
-	int index = 0;
-	int width = 0;
-	int height = 0;
-	uint8_t* pixelsData = nullptr;
+    int index = 0;
+    int width = 0;
+    int height = 0;
+    uint8_t* pixelsData = nullptr;
 
-	float translateX = 0.0f;
-	float translateY = 0.0f;
-	float scaleX = 0.0f;
-	float scaleY = 0.0f;
+    float translateX = 0.0f;
+    float translateY = 0.0f;
+    float scaleX = 0.0f;
+    float scaleY = 0.0f;
 
-	curandStateXORWOW_t* devRandStates = nullptr;
-	Camera* devCamera = nullptr;
-	Vertex* devVertices = nullptr;
-	Sphere* devSpheres = nullptr;
-	Triangle* devTriangles = nullptr;
-	Quadrilateral* devQuadrilaterals = nullptr;
-	Mesh* devMeshes = nullptr;
-	Sphere* devLights = nullptr;
-	Material* devMaterials = nullptr;
-	Texture* devTextures = nullptr;
-	uint8_t* devTexturePixels = nullptr;
-	Ray* devRays = nullptr;
-	RayHitResult* devHitResults = nullptr;
-	float* devRadiometry = nullptr;
-	uint8_t* devPixels = nullptr;
-	DeviceWorld devWorld{};
+    curandStateXORWOW_t* devRandStates = nullptr;
+    Camera* devCamera = nullptr;
+    Vertex* devVertices = nullptr;
+    Sphere* devSpheres = nullptr;
+    Triangle* devTriangles = nullptr;
+    Quadrilateral* devQuadrilaterals = nullptr;
+    Mesh* devMeshes = nullptr;
+    Sphere* devLights = nullptr;
+    Material* devMaterials = nullptr;
+    Texture* devTextures = nullptr;
+    uint8_t* devTexturePixels = nullptr;
+    Ray* devRays = nullptr;
+    RayHitResult* devHitResults = nullptr;
+    float* devRadiometry = nullptr;
+    uint8_t* devPixels = nullptr;
+    DeviceWorld devWorld{};
 
-	int devThreadNum = 256;
+    int devThreadNum = 256;
 
-	float sampleProb = 0.8f;
-	int sampleDepth = 8;
-	int sampleTimes = 32;
-	int filterKernelSize = 1;
+    float sampleProb = 0.8f;
+    int sampleDepth = 8;
+    int sampleTimes = 32;
+    int filterKernelSize = 1;
 
-	int sampleCount = 0;
-	int frame = 0;
-	int64_t timer = 0;
-	int64_t frameTime = 0;
+    int sampleCount = 0;
+    int frame = 0;
+    int64_t timer = 0;
+    int64_t frameTime = 0;
 
-	void Init();
-	void Tick(float deltaTime);
-	void Tick2(float deltaTime);
-	void TestTick(float deltaTime);
+    ~Renderer();
+
+    void Init();
+    void Tick(float deltaTime);
+    void Tick2(float deltaTime);
+    void TestTick(float deltaTime);
 };
 
 void Test();
