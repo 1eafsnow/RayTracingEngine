8b6892568e7ebebf9522554864aa3312d6b76409
1eafsnow <46445616+1eafsnow@users.noreply.github.com>
2026-09-01 15:25:16 +0800

    Optimize quadrilateral hit testing

--- src/Object/Quadrilateral.cu
+++ src/Object/Quadrilateral.cu
@@ -3,138 +3,139 @@
 
 Vertex* Quadrilateral::GetVertex(int idx)
 {
-	return &GetWorld()->vertices[vertexIdx[idx]];
+    return &GetWorld()->vertices[vertexIdx[idx]];
 }
 
 Material* Quadrilateral::GetMaterial()
 {
-	return &GetWorld()->materials[materialIdx];
+    return &GetWorld()->materials[materialIdx];
 }
 
 void Quadrilateral::Init()
 {
-	Vector3 v1 = GetVertex(1)->worldLocation - GetVertex(0)->worldLocation;
-	Vector3 v2 = GetVertex(2)->worldLocation - GetVertex(0)->worldLocation;
-	Vector3 v3 = GetVertex(3)->worldLocation - GetVertex(0)->worldLocation;
+    Vector3 v1 = GetVertex(1)->worldLocation - GetVertex(0)->worldLocation;
+    Vector3 v2 = GetVertex(2)->worldLocation - GetVertex(0)->worldLocation;
+    Vector3 v3 = GetVertex(3)->worldLocation - GetVertex(0)->worldLocation;
 
-	float angle1 = Angle(v1, v2);
-	float angle2 = Angle(v2, v3);
-	float angle3 = Angle(v1, v3);
+    const float angle1 = Angle(v1, v2);
+    const float angle2 = Angle(v2, v3);
+    const float angle3 = Angle(v1, v3);
+    const float reorderEpsilon = 1e-5f;
 
-	if (angle1 + angle3 == angle2)
-	{
-		int temp = vertexIdx[1];
-		vertexIdx[1] = vertexIdx[2];
-		vertexIdx[2] = temp;
-	}
-	else if (angle2 + angle3 == angle1)
-	{
-		int temp = vertexIdx[2];
-		vertexIdx[2] = vertexIdx[3];
-		vertexIdx[3] = temp;
-	}
+    if (fabsf((angle1 + angle3) - angle2) <= reorderEpsilon)
+    {
+        int temp = vertexIdx[1];
+        vertexIdx[1] = vertexIdx[2];
+        vertexIdx[2] = temp;
+    }
+    else if (fabsf((angle2 + angle3) - angle1) <= reorderEpsilon)
+    {
+        int temp = vertexIdx[2];
+        vertexIdx[2] = vertexIdx[3];
+        vertexIdx[3] = temp;
+    }
 
-	normal = Cross(v1, v2).GetNormalized();	
+    v1 = GetVertex(1)->worldLocation - GetVertex(0)->worldLocation;
+    v2 = GetVertex(2)->worldLocation - GetVertex(0)->worldLocation;
+    Vector3 faceNormal = Cross(v1, v2);
+    if (Dot(faceNormal, faceNormal) <= 1e-16f)
+    {
+        normal = Vector3::Zero;
+        distance = 0.0f;
+        return;
+    }
 
-	if (vertexNormal)
-	{
-		if (Dot(normal, GetVertex(0)->worldDirection + GetVertex(1)->worldDirection + GetVertex(2)->worldDirection + GetVertex(3)->worldDirection) < 0)
-		{			
-			normal = -normal;
-		}
-	}
+    normal = faceNormal.GetNormalized();
+    if (vertexNormal)
+    {
+        Vector3 averageNormal = GetVertex(0)->worldDirection + GetVertex(1)->worldDirection + GetVertex(2)->worldDirection + GetVertex(3)->worldDirection;
+        if (Dot(averageNormal, averageNormal) > 1e-16f && Dot(normal, averageNormal) < 0.0f)
+        {
+            normal = -normal;
+        }
+    }
 
-	distance = -Dot(GetVertex(0)->worldLocation, normal);
+    distance = -Dot(GetVertex(0)->worldLocation, normal);
 }
 
-Material* Quadrilateral::GetMaterial(DeviceWorld* world)
+__device__ Material* Quadrilateral::GetMaterial(DeviceWorld* world)
 {
-	return world->materials + materialIdx;
+    return world->materials + materialIdx;
 }
 
-Vector3 Quadrilateral::GetAlbedo(DeviceWorld* world, const Vector3& barycentricCoordinate)
+__device__ Vector3 Quadrilateral::GetAlbedo(DeviceWorld* world, const Vector3& barycentricCoordinate)
 {
-	return (world->materials + materialIdx)->albedo;
+    (void)barycentricCoordinate;
+    return (world->materials + materialIdx)->albedo;
 }
 
-bool Quadrilateral::IncludeDetect(DeviceWorld* world, Vector3& location)
+__device__ bool Quadrilateral::IncludeDetect(DeviceWorld* world, const Vector3& location)
 {
-	/*
-	int vIdx1 = quadrilateral->vertexIdx[0] * 3;
-	int vIdx2 = quadrilateral->vertexIdx[1] * 3;
-	int vIdx3 = quadrilateral->vertexIdx[2] * 3;
-	int vIdx4 = quadrilateral->vertexIdx[3] * 3;
-	Vector3 vLocation1(DevVertices[vIdx1], DevVertices[vIdx1 + 1], DevVertices[vIdx1 + 2]);
-	Vector3 vLocation2(DevVertices[vIdx2], DevVertices[vIdx2 + 1], DevVertices[vIdx2 + 2]);
-	Vector3 vLocation3(DevVertices[vIdx3], DevVertices[vIdx3 + 1], DevVertices[vIdx3 + 2]);
-	Vector3 vLocation4(DevVertices[vIdx4], DevVertices[vIdx4 + 1], DevVertices[vIdx4 + 2]);
-	*/
-	Vector3 vLocation1 = world->vertices[vertexIdx[0]].worldLocation;
-	Vector3 vLocation2 = world->vertices[vertexIdx[1]].worldLocation;
-	Vector3 vLocation3 = world->vertices[vertexIdx[2]].worldLocation;
-	Vector3 vLocation4 = world->vertices[vertexIdx[3]].worldLocation;
+    float referenceSide = 0.0f;
+    constexpr float edgeEpsilon = 1e-6f;
 
-	Vector3 pa = vLocation1 - location;
-	Vector3 pb = vLocation2 - location;
-	Vector3 pc = vLocation3 - location;
-	Vector3 pd = vLocation4 - location;
-
-	float angle1 = Angle(pa, pb);
-	float angle2 = Angle(pb, pc);
-	float angle3 = Angle(pc, pd);
-	float angle4 = Angle(pd, pa);
-
-	if (abs(angle1 + angle2 + angle3 + angle4 - PI * 2) > 1.0e-3)
-	{
-		return false;
-	}
-	return true;
+    for (int i = 0; i < 4; i++)
+    {
+        const Vector3& a = world->vertices[vertexIdx[i]].worldLocation;
+        const Vector3& b = world->vertices[vertexIdx[(i + 1) & 3]].worldLocation;
+        const float side = Dot(Cross(b - a, location - a), normal);
+        if (fabsf(side) <= edgeEpsilon)
+        {
+            continue;
+        }
+        if (referenceSide == 0.0f)
+        {
+            referenceSide = side;
+            continue;
+        }
+        if (referenceSide * side < 0.0f)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
-bool Quadrilateral::HitDetect(DeviceWorld* world, Ray* ray, RayHitResult* hitResult)
+__device__ bool Quadrilateral::HitDetect(DeviceWorld* world, Ray* ray, RayHitResult* hitResult)
 {
-	return false;
-	Vector3 n = normal;
-	float in = Dot(ray->direction, n);
-	float d = distance;
-	if (in == 0.0)
-	{
-		return false;
-	}
-	if (in > 0.0)
-	{
-		if ((world->materials + materialIdx)->backVisible)
-		{
-			n = -n;
-			in = -in;
-			d = -d;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	
-	float l = Dot(ray->location, n) + d;
-	float distance = -l / in;
-	if (distance < MIN_DETECT_DISTANCE || distance > hitResult->distance)
-	{
-		return false;
-	}
+    Vector3 n = normal;
+    float d = distance;
+    float in = Dot(ray->direction, n);
+    if (fabsf(in) < 1e-7f)
+    {
+        return false;
+    }
 
-	Vector3 location = ray->location + ray->direction * distance;
-	Vector3 coordinate;
+    Material* material = world->materials + materialIdx;
+    if (in > 0.0f)
+    {
+        if (!material->backVisible)
+        {
+            return false;
+        }
+        n = -n;
+        in = -in;
+        d = -d;
+    }
 
-	if (!IncludeDetect(world, location))
-	{
-		return false;
-	}
-	hitResult->isHit = true;
-	hitResult->normal = n;
-	hitResult->location = location;
-	hitResult->distance = distance;
-	hitResult->material = world->materials + materialIdx;
-	hitResult->color = GetAlbedo(world, coordinate);
+    const float rayDistance = -(Dot(ray->location, n) + d) / in;
+    if (rayDistance < MIN_DETECT_DISTANCE || rayDistance > hitResult->distance)
+    {
+        return false;
+    }
 
-	return true;
+    const Vector3 location = ray->location + ray->direction * rayDistance;
+    if (!IncludeDetect(world, location))
+    {
+        return false;
+    }
+
+    hitResult->isHit = true;
+    hitResult->normal = n;
+    hitResult->location = location;
+    hitResult->distance = rayDistance;
+    hitResult->material = material;
+    hitResult->color = material->albedo;
+    hitResult->objectId = id;
+    return true;
 }
--- src/Object/Quadrilateral.h
+++ src/Object/Quadrilateral.h
@@ -9,22 +9,22 @@ class DeviceWorld;
 class Quadrilateral
 {
 public:
-	int id;
-	int vertexIdx[4];
-	int normalIdx[4];
-	bool vertexNormal;
-	Vector3 normal;
-	float distance;
-	int materialIdx;
+    int id;
+    int vertexIdx[4];
+    int normalIdx[4];
+    bool vertexNormal;
+    Vector3 normal;
+    float distance;
+    int materialIdx;
 
-	Vertex* GetVertex(int idx);
-	Normal* GetVertexNormal(int idx);
-	Material* GetMaterial();
+    Vertex* GetVertex(int idx);
+    Normal* GetVertexNormal(int idx);
+    Material* GetMaterial();
 
-	void Init();
+    void Init();
 
-	__device__ Material* GetMaterial(DeviceWorld* world);
-	__device__ Vector3 GetAlbedo(DeviceWorld* world, const Vector3& barycentricCoordinate);
-	__device__ bool IncludeDetect(DeviceWorld* world, Vector3& location);
-	__device__ bool HitDetect(DeviceWorld* world, Ray* ray, RayHitResult* hitResult);
+    __device__ Material* GetMaterial(DeviceWorld* world);
+    __device__ Vector3 GetAlbedo(DeviceWorld* world, const Vector3& barycentricCoordinate);
+    __device__ bool IncludeDetect(DeviceWorld* world, const Vector3& location);
+    __device__ bool HitDetect(DeviceWorld* world, Ray* ray, RayHitResult* hitResult);
 };
