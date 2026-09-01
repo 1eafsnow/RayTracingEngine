afac5384a37146297f8f73b0914b2b21899dbfa0
1eafsnow <46445616+1eafsnow@users.noreply.github.com>
2026-09-01 14:13:28 +0800

    Expose triangle BVH data to CUDA world

--- src/World/World.h
+++ src/World/World.h
@@ -7,76 +7,81 @@
 #include <Object/Model.h>
 #include <Material/Texture.h>
 #include <Material/Material.h>
+#include <Render/BVH.h>
 #include <vector>
 #include <chrono>
 
 class World
 {
 public:
-	std::vector<Vertex> vertices;
-	std::vector<Sphere> spheres;
-	std::vector<Triangle> triangles;
-	std::vector<Quadrilateral> quadrilaterals;
-	std::vector<Mesh> meshes;
-	std::vector<Sphere> lights;
-	std::vector<Material> materials;
-	std::vector<Texture> textures;
-	std::vector<uint8_t> texturePixels;
+    std::vector<Vertex> vertices;
+    std::vector<Sphere> spheres;
+    std::vector<Triangle> triangles;
+    std::vector<Quadrilateral> quadrilaterals;
+    std::vector<Mesh> meshes;
+    std::vector<Sphere> lights;
+    std::vector<Material> materials;
+    std::vector<Texture> textures;
+    std::vector<uint8_t> texturePixels;
 
-	int objectCount = 0;
+    int objectCount = 0;
 
-	int CreateVertex(Vector3 location = Vector3::Zero);
-	Vertex* GetVertex(int idx);
+    int CreateVertex(Vector3 location = Vector3::Zero);
+    Vertex* GetVertex(int idx);
 
-	int CreateSphere(Vector3 location = Vector3::Zero, float radius = 1.0f, int materialIdx = -1);
-	Sphere* GetSphere(int idx);
+    int CreateSphere(Vector3 location = Vector3::Zero, float radius = 1.0f, int materialIdx = -1);
+    Sphere* GetSphere(int idx);
 
-	int CreateTriangle(Vector3I vertexIdx = Vector3I(-1, -1, -1), int materialIdx = -1);
-	Triangle* GetTriangle(int idx);
+    int CreateTriangle(Vector3I vertexIdx = Vector3I(-1, -1, -1), int materialIdx = -1);
+    Triangle* GetTriangle(int idx);
 
-	int CreateQuadrilateral(Vector4I vertexIdx = Vector4I(-1, -1, -1, -1), int materialIdx = -1);
-	Quadrilateral* GetQuadrilateral(int idx);
+    int CreateQuadrilateral(Vector4I vertexIdx = Vector4I(-1, -1, -1, -1), int materialIdx = -1);
+    Quadrilateral* GetQuadrilateral(int idx);
 
-	int CreateMesh(int materialIdx = -1);
-	Mesh* GetMesh(int idx);
+    int CreateMesh(int materialIdx = -1);
+    Mesh* GetMesh(int idx);
 
-	int CreateBox(int materialIdx = -1);
-	Mesh* GetBox(int idx);
+    int CreateBox(int materialIdx = -1);
+    Mesh* GetBox(int idx);
 
-	int CreateLight();
-	Sphere* GetLight(int idx);
+    int CreateLight();
+    Sphere* GetLight(int idx);
 
-	int CreateMaterial();
-	Material* GetMaterial(int idx);
+    int CreateMaterial();
+    Material* GetMaterial(int idx);
 
-	int CreateTexture(const char* path);
-	Texture* GetTexture(int idx);
+    int CreateTexture(const char* path);
+    Texture* GetTexture(int idx);
 
-	int CreateTexturePixel(int size);
-	uint8_t* GetTexturePixel(int idx);
+    int CreateTexturePixel(int size);
+    uint8_t* GetTexturePixel(int idx);
 };
 
 class DeviceWorld
 {
 public:
-	Vertex* vertices;
-	int verticesSize;
-	Sphere* spheres;
-	int spheresSize;
-	Triangle* triangles;
-	int trianglesSize;
-	Quadrilateral* quadrilaterals;
-	int quadrilateralsSize;
-	Mesh* meshes;
-	int meshesSize;
-	Sphere* lights;
-	int lightsSize;
-	Material* materials;
-	int materialsSize;
-	Texture* textures;
-	int texturesSize;
-	uint8_t* texturePixels;
-	int texturePixelsSize;
+    Vertex* vertices = nullptr;
+    int verticesSize = 0;
+    Sphere* spheres = nullptr;
+    int spheresSize = 0;
+    Triangle* triangles = nullptr;
+    int trianglesSize = 0;
+    Quadrilateral* quadrilaterals = nullptr;
+    int quadrilateralsSize = 0;
+    Mesh* meshes = nullptr;
+    int meshesSize = 0;
+    Sphere* lights = nullptr;
+    int lightsSize = 0;
+    Material* materials = nullptr;
+    int materialsSize = 0;
+    Texture* textures = nullptr;
+    int texturesSize = 0;
+    uint8_t* texturePixels = nullptr;
+    int texturePixelsSize = 0;
+    BVHNode* bvhNodes = nullptr;
+    int bvhNodesSize = 0;
+    int* bvhTriangleIndices = nullptr;
+    int bvhTriangleIndicesSize = 0;
 };
 
 World* GetWorld();
